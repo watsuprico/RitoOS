4316e2281197c0759624249d6bbcc0763bdd60ab
Watsuprico <codynewberry3@gmail.com>
2015-12-10 21:07:06 -0500

    Alpha Update 0006 - 0008
    MASSIVE OVERHAUL!
    
    This update not only includes 0008, but 0006 and 0007. Why? Well, it
    seems I didn't upload the changes I was making on 0005, which soon
    became 0006, then 0007, and out of no were, 0008.
    This is most likely due to me being busy/doing other things then develop
    for this OS. Woops...
    
    Any ways, this is what I think I did for 0006:
    
    01; Fix the BSOD's
    02; A few slight bug fixes
    03; Boot.lua updates
    04; Start development on a Lockdown API
    
    - 0006 didn't include much -
    
    0007:
    
    01; Bug fixes.
    02; Fix animations for OS shutdown/restart
    03; Add boot animations
    04; Fix the labels RitoOS sets. (They now show the state the computer is
    in) ((Ex: booting, off, recovery, etc)
    05; Add userEnvironment.rxf - This will be the bridge between the user
    and the system
    06; Strip RitoOS
    07; And recoveryScript function. (Like Android's Open Script Recovery)
    08; Added a new animation when logging in.
    
    0008:
    
    01; Update the recovery program. (New look major bug fixes)
    02; Shorten the code needed for the animations in boot and
    restart/shutdown
    03; Add non-advance computer support. (Known as "silver mode". Turns all
    colors into some shade of black, white, gray, or lightGray.)
    04; Add a new power manager. (pwr)
    05; Update CMD to allow for predict typing. (Similar to CC's shell
    program)

--- System/.Boot/Boot.lua
+++ System/.Boot/Boot.lua
@@ -6,7 +6,7 @@
     |                                                                         |
     | - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - |
     |                                                                         |
-    |                 Boot File Version Alpha 2.0.1 (B005)                    |
+    |                 Boot File Version Alpha 2.0.1 (B008)                    |
     |                                                                         |
     |  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  |
     |                                                                         |
@@ -41,7 +41,16 @@
 local devMode = false --Does nothing... for now.
 
 
-local ver = "RitoOS Alpha 2.0.1; build 005"
+local bootScreen = true -- False allows for an instant boot, and removes the boot animation
+
+shutdownAnimation = true -- False speeds up shutdowns by 0.3 seconds, and removes the animation. (Similar to shutting down via recovery) (Also effects reboots)
+shutdownWaitTime = 0.05 -- !SETTING THIS INCORRECTLY CAN F*%k YOUR SYSTEM! The time the system will wait in between messages on the shutdown / reboot screen(s). (Set this number to 0 and turn off shutdown animations for instant reboots and shutdowns)
+
+bw = false -- Run in black and white mode?
+
+local _tasks = {}
+
+local ver = "RitoOS Alpha 2.0.1; build 008"
 _G.RitoOS_CC_Version = os.version() -- Computercraft version
 
 local function getTable(path)
@@ -64,11 +73,11 @@ end
 -- Services
 local function startConfigService()
     System.logInfo("System","Starting config service.")
-    local ok, err = pcall(function() dofile('/System/Services/Config/Config.serv') end)
+    local ok, err = pcall(function() dofile("/System/Services/Config/Config.serv") end)
     if ok then
         System.logInfo("System","Started Config service.")
     else
-        System.logAlert("System","Error: Config serivce either couldn't load, or crashed. Error code: "..err)
+        System.logAlert("System","Error: Config service either couldn't load, or crashed. Error code: "..err)
     end
 end
 local function startMonitorService()
@@ -77,7 +86,7 @@ local function startMonitorService()
     if ok then
         System.logInfo("System","Started Monitor service.")
     else
-        System.logAlert("System","Error: Monitor serivce either couldn't load, or crashed. Error code: "..err)
+        System.logAlert("System","Error: Monitor service either couldn't load, or crashed. Error code: "..err)
     end
 end
 local function startUpdateService()
@@ -86,7 +95,7 @@ local function startUpdateService()
     if ok then
         System.logInfo("System","Started Update service.")
     else
-        System.logAlert("System","Error: Update serivce either couldn't load, or crashed. Error code: "..err)
+        System.logAlert("System","Error: Update service either couldn't load, or crashed. Error code: "..err)
     end
 end
 local function startRitLock()
@@ -95,17 +104,16 @@ local function startRitLock()
     if ok then
         System.logInfo("System","Started RitLock service.")
     else
-        System.logAlert("System","Error: RitLock serivce either couldn't load, or crashed. Error code: "..err)
+        System.logAlert("System","Error: RitLock service either couldn't load, or crashed. Error code: "..err)
     end
 end
 local function startOsFunction()
-    sleep(0.2)
     System.logInfo("System","Starting os function list.")
     local ok, err = pcall(function() dofile('/System/Services/OS/OS.serv') end)
     if ok then
         System.logInfo("System","Started OS Function service.")
     else
-        System.logAlert("System","Error: OS Function serivce either couldn't load, or crashed. Error code: "..err)
+        System.logAlert("System","Error: OS Function service either couldn't load, or crashed. Error code: "..err)
     end
 end
 local function startBackupService()
@@ -121,19 +129,11 @@ end
 
 -- Main
 local function main()
-    sleep(0)
-    term.setBackgroundColor(colors.gray)
-    term.clear()
-    sleep(0)
-    term.setBackgroundColor(colors.lightGray)
-    term.clear()
-    sleep(0)
-    term.setBackgroundColor(colors.white)
-    term.clear()
     _G.shell = shell -- Make shell.run() usable
     _G.fs = fs --Make fs usable
     _G.RitoOS = true -- We're booting
     _G.RitoOS_Version = ver -- Our version
+    RitoOS = true -- We're running!
 
     -- Load a few APIs
     if os.loadAPI("/System/APIs/System/System") then
@@ -152,7 +152,7 @@ local function main()
             BNi = tonumber(string.sub(BN[1],string.find(BN[1],"!")+1))
             if BNi >= 3 then
                 System.logWarn("System","Possibly damaged, booting into recovery")
-                RitoOS_bootIntoRecovery = true
+                _RitoOS_bootIntoRecovery = true
             else
                 BNi = BNi + 1
                 local file = assert(io.open("/System/.Boot/BN", "w"))
@@ -164,24 +164,134 @@ local function main()
             file:write("!1")
             file:close()
         end
+    else
+        local UpdateStartupFile = assert(io.open("/Startup", "w"))
+        UpdateStartupFile:write(System.startupFile())
+        UpdateStartupFile:close()
     end
     
     if _RitoOS_bootIntoRecovery == true then
         System.logAlert("System","Entering recovery mode.")
+        os.setComputerLabel("[Recovery] - RitoOS")
+        SystemBootIntoRecovery = true
         dofile("/System/.Recovery/.Recover")
     end
 
     if Update_Service_completeOnBoot then
         Complete_Update = true
+        os.setComputerLabel("[Updating] - RitoOS")
         dofile("/System/Services/Update/Update.serv")
     end
+    
     --Setup the main window
     OldTerm = term.current()
     local maxX, maxY = term.getSize()
     Windows_Main = window.create(term.current(),1,1,maxX,maxY,true)
     term.redirect(Windows_Main)
+
+    os.setComputerLabel("[BOOTING] - RitoOS")
+
+    System.logInfo("System","Showing boot logo")
+
+    if bootScreen then
+        parallel.waitForAll(function() -- Boot screen
+            System.aniT(colors.gray,0)
+        end, function()
+            System.aniB(colors.gray,0)
+        end, function()
+            System.aniT(colors.lightGray,0.043)
+        end, function()
+            System.aniB(colors.lightGray,0.038)
+        end, function()
+            sleep(0.083)
+            local _,Y = term.getSize()
+            local Y = Y/2
+            while Y >= 0 do
+                term.setCursorPos(1,Y)
+                term.setBackgroundColor(colors.white)
+                term.clearLine()
+                term.setTextColor(colors.black)
+                local X3,Y3 = term.getSize()
+                term.setCursorPos(math.floor(X3-string.len("RitoOS"))/2, Y3/2-1)
+                print("RitoOS")
+                term.setCursorPos(math.floor(X3-string.len("Hold R for recovery"))/2, Y3/2+1)
+                term.setTextColor(colors.gray)
+                print("Hold R for recovery")
+                Y = Y-1
+                sleep(0)
+            end
+        end, function()
+            System.aniB(colors.white,0.078)
+        end)
+    else
+        term.setBackgroundColor(colors.white)
+        term.clear()
+    end
+
+    parallel.waitForAny(function()
+        local X3,Y3 = term.getSize()
+        term.setCursorPos(math.floor(X3-string.len("Hold R for recovery"))/2, Y3/2+1)
+        term.setTextColor(colors.orange)
+        term.setBackgroundColor(colors.white)
+        print("Hold R for recovery")
+        local event, key = os.pullEvent("key")
+        while true do
+            if key == keys.r then
+                local X3,Y3 = term.getSize()
+                term.setCursorPos(math.floor(X3-string.len("Please wait..."))/2, Y3/2-1)
+                term.setTextColor(colors.black)
+                term.setBackgroundColor(colors.white)
+                term.clear()
+                print("Please wait...")
+                Reboot_To_Recovery = true
+                break
+            end
+        end
+    end,function()
+        sleep(0.2)
+        local X3,Y3 = term.getSize()
+        term.setCursorPos(math.floor(X3-string.len("Hold R for recovery"))/2, Y3/2+1)
+        term.setTextColor(colors.gray)
+        term.setBackgroundColor(colors.white)
+        print("Hold R for recovery")
+        end)
+
+    if Reboot_To_Recovery == true then
+        local X3,Y3 = term.getSize()
+        term.setCursorPos(math.floor(X3-string.len("Rebooting..."))/2, Y3/2)
+        term.setTextColor(colors.black)
+        term.setBackgroundColor(colors.white)
+        term.clearLine()
+        print("Rebooting...")
+        RitoOS_bootIntoRecovery = true
+        sleep(0.5)
+        os.reboot(0, true)
+    end
+
+
     -- Load APIs
     System.logInfo("System","Loading APIs.")
+
+    System.logInfo("System","Loading multitask API.")
+    if os.loadAPI("/System/APIs/Multitask/Multitask") then
+        _G.monitor = Monitor
+        System.logInfo("System","Multitask API loaded.")
+    else
+        if not err then
+            err = "No error code ):"
+        end
+        System.logAlert("System","Error: Multitask API failed to load or crashed. Error: "..err)
+    end
+    
+    System.logInfo("System","Loading Bedrock API.")
+    if os.loadAPI("/System/APIs/Bedrock/Bedrock") then
+        System.logInfo("System","Bedrock API loaded.")
+    else
+        if not err then
+            err = "No error code ):"
+        end
+        System.logAlert("System","Error: Bedrock API failed to load or crashed. Error: "..err)
+    end
 
     System.logInfo("System","Loading monitor API.")
     if os.loadAPI("/System/APIs/Monitor/Monitor") then
@@ -210,50 +320,43 @@ local function main()
     local function finishBoot()
         --Set a few more vars
         System.logInfo("System","Setting computer label")
-        os.setComputerLabel(RitoOS_Computer_Label)
+        os.setComputerLabel(RitoOS_Name)
         System.logInfo("System","Loading CodeX")
-        local ok, err = pcall(function() term.setBackgroundColor(colors.black) term.clear() dofile("/System/CodeX.rxf") end)
+        local ok, err = pcall(function() dofile("/System/CodeX.rxf") end)
         if not ok then
             if not err then
-                err = "No error code! Oh noes! The crash point, however, branched off of line 179 in the boot file."
+                err = "No error code! Oh noes! The crash point, however, branched off of line 251 in the boot file. (dofile('/System/CodeX.rxf'))."
             end
             System.logAlert("System PANIC","Oh noes! We crashed! D:")
             System.logAlert("System PANIC","What caused this? Well, I don't know... but this does relate to the system crashing: ' "..err.." '.")
             System.logAlert("System PANIC","Please, send that error message to Watsuprico via PM. He'll tell you how to fix it, when it'll be patched, or at least what it means.")
             System.logAlert("System PANIC","Showing the BSOD message, nothing much I can do.")
-            configfiledata = "Sorry, I couldn't retrieve that data ): "
+            if RitoOS_Config_service_started then
+                configfileran = "Yes"
+            else
+                configfileran = "Unknown, but probably not."
+            end
             local crash = assert(io.open("/System/Latest.log", "a"))
-            crash:write("\n^ System log file ^\n\nRitoOS crashed.\nHere's some of that juicy debugging info.\n\nCrash info: \nError: "..err.." \n\nGeneral system info: \nCC version: "..RitoOS_CC_Version.."\nRitoOS version: "..RitoOS_Version.."\nConfig file: "..configfiledata.."\n\n\nEnd of the crash log.\n\n")
+            crash:write("\n^ System log file ^\n\nRitoOS crashed.\nHere's some of that juicy debugging info.\n\nCrash info: \nError: "..err.." \n\nGeneral system info: \nCC version: "..RitoOS_CC_Version.."\nRitoOS version: "..RitoOS_Version.."\nConfig file ran: "..configfileran.."\n\n\nEnd of the crash log.\n\n")
             crash:close()
             if fs.exists("/System/crash.log") then
                 fs.delete("/System/crash.log")
             end
                 fs.copy("/System/Latest.log","/System/crash.log")
             local x,Y = term.getSize()
-            term.setTextColor(colors.white)
-            term.setBackgroundColor(colors.white)
-            term.clear()
-            sleep(0)
-            term.setBackgroundColor(colors.lightGray)
-            term.clear()
-            sleep(0)
-            term.setBackgroundColor(colors.gray)
-            term.clear()
-            sleep(0)
             term.setBackgroundColor(colors.black)
             term.clear()
-            term.setCursorPos(math.floor(x-string.len("This isn't too good..."))/2, math.floor(Y/2))
-            print("That isn't too good...")
-            sleep(0.1)
             term.setBackgroundColor(colors.gray)
             term.clear()
             sleep(0)
             term.setBackgroundColor(colors.lightGray)
-            term.setTextColor(colors.brown)
+            term.setTextColor(colors.red)
             term.clear()
+            sleep(0)
             term.setBackgroundColor(colors.white)
             term.clear()
             sleep(0)
+            term.setTextColor(colors.red)
             term.setCursorPos(math.floor(x-string.len('Sorry about that :('))/2, 2)
             write("Sorry about that. ): ")
             term.setTextColor(colors.black)
@@ -272,7 +375,9 @@ local function main()
             term.setCursorPos(math.floor(x-string.len('.include the log file at "/System/Latest.log"'))/2, 12)
             print("include the log file at: '/System/Latest.log'")
             print()
-            print("Press enter to try and save the system.")
+            if not attemptToSave then
+                print("Press enter to try and save the system.")
+            end
             print("Press R to go into recovery.")
             print("Press any other key to shutdown.")
             sleep(1)
@@ -302,7 +407,7 @@ local function main()
     end
     --Start services
     System.logInfo("System","Starting services.")
-    parallel.waitForAll(startBackupService,startConfigService,startMonitorService,startUpdateService,startRitLock,finishBoot)
+    parallel.waitForAll(startConfigService,startBackupService,startMonitorService,startUpdateService,startRitLock,finishBoot)
 end
 
 
@@ -318,12 +423,18 @@ local function TimeKeeper()
 end
 
 local function VersionInfo()
-    function os.version()
-        return ver
+    while true do
+        sleep(0)
+        function os.version()
+            return ver
+        end
     end
 end
 
 if not RitoOS then
+    if term.isColor == nil then
+        error("I'm sorry, RitoOS does not support computer versions 1.7 or less! Please upgrade!")
+    end
     parallel.waitForAll(TimeKeeper,main,startOsFunction,VersionInfo)
 else
     error("RitoOS already running.")
